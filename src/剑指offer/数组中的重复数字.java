74fc37222a3335fa0b1240fc3309e4ff15fc4639
LiJunjun <497601974@qq.com>
2019-11-21 15:30:55 +0800

    first push git project from idea   newcode practive
      尝试添加修改并提交到github

---
+++ src/剑指offer/数组中的重复数字.java
@@ -0,0 +1,12 @@
+package 剑指offer;
+
+public class 数组中的重复数字 {
+
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+
+        boolean flag = false;
+
+
+        return flag;
+    }
+}
